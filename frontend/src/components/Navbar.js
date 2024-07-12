150cbadb1c1f17bac45dbe15a08a3db1dd04aea7
Amir Bibi <amirbi595@gmail.com>
2024-07-12 16:03:39 +0300

    #13 - Setting the Initial Auth Status

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
 import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 const Navbar = () => {
   const { logout } = useLogout();
+  const { user } = useAuthContext();
+
   const handleLogout = () => {
     logout();
   }
@@ -13,13 +16,17 @@ const Navbar = () => {
           <h1>Workout Buddy</h1>
         </Link>
         <nav>
-          <div>
-            <button onClick={handleLogout}>Logout</button>
-          </div>
-          <div>
-            <Link to="./login">Login</Link>
-            <Link to="./signup">Signup</Link>
-          </div>
+          {user ? (
+            <div>
+              <span>{user.email}</span>
+              <button onClick={handleLogout}>Logout</button>
+            </div>
+          ) :
+            <div>
+              <Link to="./login">Login</Link>
+              <Link to="./signup">Signup</Link>
+            </div>
+          }
         </nav>
       </div>
     </header>
--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,4 +1,4 @@
-import { createContext, useReducer } from "react";
+import { createContext, useEffect, useReducer } from "react";
 
 export const AuthContext = createContext();
 
@@ -20,6 +20,14 @@ export const AuthContextProvider = ({ children }) => {
     user: null
   })
 
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('user'))
+
+    if (user) {
+      dispatch({ type: 'LOGIN', payload: user })
+    }
+
+  }, [])
   console.log("AuthContext state: ", state)
 
   return (
